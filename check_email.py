112ee3a5acaf9b81341e6d12976e93936b8adce3
gret13 <115559960+gret13@users.noreply.github.com>
2022-10-12 09:50:42 +0500

    Update check_email.py

--- check_email.py
+++ check_email.py
@@ -2,7 +2,7 @@ def check_email(email):
     email_lower = email.lower()
     if '@yandex' and not '@yandex-team' in email_lower:
         email_lower.replace('@yandex', '@ya')
-        return email_lower
+    return email_lower
 
 
 assert check_email('m1@Yandex.ru') == 
